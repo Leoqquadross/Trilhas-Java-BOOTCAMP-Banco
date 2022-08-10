95cfa63aa4349318a1a3e623670b8b3bf7137e6c
Leonardo Quadros <leonardoq@ciandt.com>
2022-08-10 16:52:02 -0300

    Banco

---
+++ java_jpa_hibernate_mapeamento/gestao-cursos-api/src/main/java/demo/GestaoCursosMain.java
@@ -0,0 +1,86 @@
+package demo;
+
+import entities.*;
+import models.*;
+
+import java.time.LocalDate;
+import java.util.List;
+
+public class GestaoCursosMain {
+    public static void main(String[] args) {
+        // Persistindo dados de Aluno
+        Aluno aluno = new Aluno();
+
+        aluno.setEmail("leo");
+        aluno.setMatricula("2002");
+        aluno.setNomeCompleto("Ola");
+        aluno.setNascimento(LocalDate.of(1998, 01, 01));
+        aluno.setEnderecos(null);
+        aluno.setTelefones(null);
+        AlunoModel alunoModel = new AlunoModel();
+
+        alunoModel.create(aluno);
+
+        // Persistindo dados de Endereco
+
+        Endereco endereco = new Endereco();
+        endereco.setEndereco("RUa a ");
+        endereco.setBairro("Casass");
+        endereco.setCidade("ssa");
+        endereco.setBairro("ba");
+        endereco.setLogradouro("casa");
+        endereco.setNumero("15");
+
+        EnderecoModel enderecoModel = new EnderecoModel();
+
+        enderecoModel.create(endereco);
+
+        aluno.setEnderecos(List.of(endereco));
+        alunoModel.update(aluno);
+
+        // Persistindo dados de MaterialCurso
+
+        MaterialCurso materialC = new MaterialCurso();
+        materialC.setUrl("www.tech.com.br");
+
+        MaterialModel materiaM = new MaterialModel();
+        materiaM.create(materialC);
+
+        // Professor
+        Professor professor = new Professor();
+        professor.setEmail("aa");
+        professor.setMatricula("bb");
+        professor.setNomeCompleto("cc");
+
+        ProfessorModel professorModel = new ProfessorModel();
+        professorModel.create(professor);
+
+        // Persistindo dados de Curso
+
+        Curso curso = new Curso();
+        curso.setNome("Matematica");
+        curso.setSigla("MAT");
+        curso.setMaterialCurso(materialC);
+        curso.setProfessor(professor);
+        curso.setAlunos(List.of(aluno));
+        CursoModel cursoModel = new CursoModel();
+        cursoModel.create(curso);
+
+        // Persistindo dados de Aluno_Telefone
+
+        Telefone telefone = new Telefone();
+        telefone.setNumero("988997311");
+        telefone.setDDD("71");
+        telefone.setAluno(aluno);
+
+        TelefoneModel telefoneModel = new TelefoneModel();
+        telefoneModel.create(telefone);
+        aluno.setTelefones(List.of(telefone));
+        alunoModel.update(aluno);
+
+        aluno.setCursos(List.of(curso));
+        alunoModel.update(aluno);
+
+
+    }
+}
